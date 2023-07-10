daa94c4e8ce14c0c541794e1a41f6b6a3a2f6ee8
Tepokun <artememelyanow@gmail.com>
2023-07-10 22:04:39 +0300

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -4,8 +4,8 @@ const clickerCounter = document.getElementById("clicker__counter");
 cookie.onclick = function() {
     let clicks = clickerCounter.textContent++;
     if (clicks % 2 == 0) {
-        cookie.width = 150;
-    } else {
         cookie.width = 200;
+    } else {
+        cookie.width = 300;
     }
 }
