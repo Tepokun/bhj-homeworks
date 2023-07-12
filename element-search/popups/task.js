8c4644e4c29e71a9737025f97fa53acf078608b9
Tepokun <artememelyanow@gmail.com>
2023-07-13 00:23:56 +0300

    Update task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,26 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var modalMain = document.getElementById('modal_main');
-    var modalSuccess = document.getElementById('modal_success');
-    var modalCloseButtons = document.querySelectorAll('.modal__close');
-    var showSuccessButton = document.querySelector('.show-success');
-  
-    modalMain.classList.add('modal_active');
-  
-    for (var i = 0; i < modalCloseButtons.length; i++) {
-      modalCloseButtons[i].addEventListener('click', closeModal);
-    }
-  
-    showSuccessButton.addEventListener('click', showSuccessModal);
-  
-    function closeModal() {
-      var modal = this.closest('.modal');
-      modal.classList.remove('modal_active');
-    }
-  
-    function showSuccessModal(event) {
-      event.preventDefault();
-      modalMain.classList.remove('modal_active');
-      modalSuccess.classList.add('modal_active');
-    }
-  });
-  
+  var modalMain = document.getElementById('modal_main');
+  var modalSuccess = document.getElementById('modal_success');
+  var modalCloseButtons = document.querySelectorAll('.modal__close');
+  var showSuccessButton = document.querySelector('.show-success');
+
+  modalMain.classList.add('modal_active');
+
+  for (var i = 0; i < modalCloseButtons.length; i++) {
+    modalCloseButtons[i].addEventListener('click', closeModal);
+  }
+
+  showSuccessButton.addEventListener('click', showSuccessModal);
+
+  function closeModal() {
+    var modal = this.closest('.modal');
+    modal.classList.remove('modal_active');
+  }
+
+  function showSuccessModal(event) {
+    event.preventDefault();
+    modalMain.classList.remove('modal_active');
+    modalSuccess.classList.add('modal_active');
+  }
+});
