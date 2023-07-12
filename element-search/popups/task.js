8ed5e6193bb102b0c95bee8d0dbdc1c7971aa70f
Tepokun <artememelyanow@gmail.com>
2023-07-13 00:48:24 +0300

    Update task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,25 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var modalMain = document.getElementById('modal_main');
-  var modalSuccess = document.getElementById('modal_success');
-  var modalCloseButtons = document.querySelectorAll('.modal__close');
-  var showSuccessButton = document.querySelector('.show-success');
-
-  modalMain.classList.add('modal_active');
-
-  for (var i = 0; i < modalCloseButtons.length; i++) {
-    modalCloseButtons[i].addEventListener('click', closeModal);
-  }
-
-  showSuccessButton.addEventListener('click', showSuccessModal);
-
-  function closeModal() {
-    var modal = this.closest('.modal');
-    modal.classList.remove('modal_active');
-  }
-
-  function showSuccessModal(event) {
-    event.preventDefault();
-    modalMain.classList.remove('modal_active');
-    modalSuccess.classList.add('modal_active');
-  }
-});
+    var modalMain = document.getElementById('modal_main');
+    var modalSuccess = document.getElementById('modal_success');
+    var modalCloseButtons = document.querySelectorAll('.modal__close');
+    var showSuccessButton = document.querySelector('.show-success');
+    var makeGoodButton = document.querySelector('.btn_danger');
+  
+    modalMain.classList.add('modal_active');
+  
+    function showModal(modal) {
+      modal.classList.add('modal_active');
+    }
+  
+    function hideModal(modal) {
+      modal.classList.remove('modal_active');
+    }
+  
+    for (var i = 0; i < modalCloseButtons.length; i++) {
+      modalCloseButtons[i].addEventListener('click', function() {
+        hideModal(this.closest('.modal'));
+      });
+    }
+  
+    showSuccessButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      hideModal(modalMain);
+      showModal(modalSuccess);
+      setTimeout(function() {
+        hideModal(modalSuccess);
+      }, 3000);
+    });
+  
+    makeGoodButton.addEventListener('click', function(event) {
+      event.preventDefault();
+      showModal(modalSuccess);
+      setTimeout(function() {
+        hideModal(modalMain);
+        hideModal(modalSuccess);
+      }, 3000);
+    });
+  });
+  
