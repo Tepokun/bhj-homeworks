87728ebdfa03230218b954228f8052c18829727e
Tepokun <artememelyanow@gmail.com>
2023-07-17 10:56:19 +0300

    Update task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,38 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var modalMain = document.getElementById('modal_main');
-    var modalSuccess = document.getElementById('modal_success');
-    var modalCloseButtons = document.querySelectorAll('.modal__close');
-    var showSuccessButton = document.querySelector('.show-success');
-    var makeGoodButton = document.querySelector('.btn_danger');
-  
-    modalMain.classList.add('modal_active');
-  
-    function showModal(modal) {
-      modal.classList.add('modal_active');
-    }
-  
-    function hideModal(modal) {
-      modal.classList.remove('modal_active');
-    }
-  
-    modalCloseButtons.forEach(function(button) {
-      button.addEventListener('click', function() {
-        hideModal(this.closest('.modal'));
-      });
-    });
-  
-    showSuccessButton.addEventListener('click', function(event) {
-      event.preventDefault();
-      hideModal(modalMain);
-      showModal(modalSuccess);
-      setTimeout(function() {
-        hideModal(modalSuccess);
-      }, 3000);
-    });
-  
-    makeGoodButton.addEventListener('click', function(event) {
-      event.preventDefault();
-      showModal(modalSuccess);
-    });
-  });
-  
+const popup = document.querySelector('#modal_main');
+const close = document.querySelectorAll('div.modal__close');
+const successBtn = document.querySelector('.show-success');
+const success = document.querySelector('#modal_success');
+
+
+popup.className = 'modal modal_active';
+
+close[0].onclick = function () {
+    popup.className = 'modal';
+};
+
+successBtn.onclick = function () {
+    success.className = 'modal modal_active';
+};
+
+close[1].onclick = function () {
+    popup.className = 'modal';
+    success.className = 'modal';
+};
