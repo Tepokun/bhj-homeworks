e593188eea74c1ed883a4394c60d472a02f2301e
Tepokun <artememelyanow@gmail.com>
2023-08-09 20:44:49 +0300

    Update task.js

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,34 +1,36 @@
 const signinForm = document.getElementById('signin__form');
-const signinBtn = document.getElementById('signin__btn');
+const signinDiv = document.getElementById('signin');
 const welcomeBlock = document.getElementById('welcome');
 const welcomeUserId = document.getElementById('user_id');
 const userId = localStorage.getItem('user_id');
 if (userId) {
-  welcomeUserId.textContent = userId;
-  welcomeBlock.classList.add('welcome_active');
+    welcomeUserId.textContent = userId;
+    welcomeBlock.classList.add('welcome_active');
+    signinDiv.classList.remove('signin_active');
 }
 
 signinForm.addEventListener('submit', async (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const formData = new FormData(signinForm);
+    const formData = new FormData(signinForm);
 
-  try {
-    const response = await fetch('https://students.netoservices.ru/nestjs-backend/auth', {
-      method: 'POST',
-      body: formData
-    });
+    try {
+        const response = await fetch('https://students.netoservices.ru/nestjs-backend/auth', {
+            method: 'POST',
+            body: formData
+        });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    if (data.success) {
-      localStorage.setItem('user_id', data.user_id);
-      welcomeUserId.textContent = data.user_id;
-      welcomeBlock.classList.add('welcome_active');
-    } else {
-      alert('Неверный логин/пароль');
+        if (data.success) {
+            localStorage.setItem('user_id', data.user_id);
+            welcomeUserId.textContent = data.user_id;
+            welcomeBlock.classList.add('welcome_active');
+            signinDiv.classList.remove('signin_active');
+        } else {
+            alert('Неверный логин/пароль');
+        }
+    } catch (error) {
+        console.error('Произошла ошибка при авторизации:', error);
     }
-  } catch (error) {
-    console.error('Произошла ошибка при авторизации:', error);
-  }
 });
